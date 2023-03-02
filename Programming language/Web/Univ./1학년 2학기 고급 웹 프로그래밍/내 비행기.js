ea982ea1d3d75a3798a177582f6eddc1744301a7
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2023-03-02 15:55:52 +0900

    Create 내 비행기.js

---
+++ Programming language/Web/Univ./1학년 2학기 고급 웹 프로그래밍/내 비행기.js
@@ -0,0 +1,53 @@
+function drawShip() {
+    if (stype === 1) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(x + 40, y - 5, 20, 10);
+    }
+    if (stype > 1) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(x + 10, y - 15, 10, 30);
+    }
+    if (stype > 2) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(x, y - 20, 10, 40);
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(x - 15, y);
+    ctx.lineTo(x, y - 15);
+    ctx.lineTo(x + 60, y);
+    ctx.lineTo(x, y + 15);
+    ctx.closePath();
+    ctx.fillStyle="red";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x + 60, y);
+    ctx.lineTo(x + 40, y - 4);
+    ctx.lineTo(x + 40, y + 4);
+    ctx.closePath();
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x - 30, y - 8);
+    ctx.lineTo(x - 10, y - 8);
+    ctx.lineTo(x - 10, y - 13);
+    ctx.lineTo(x - 15, y - 13);
+    ctx.closePath();
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(x - 30, y + 8);
+    ctx.lineTo(x - 10, y + 8);
+    ctx.lineTo(x - 10, y + 13);
+    ctx.lineTo(x - 15, y + 13);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(x + 10, y, 5, 0, Math.PI * 2);
+    ctx.fillStyle = "skyblue";
+    ctx.fill();
+}
